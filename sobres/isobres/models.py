36395a4ea7dc5908f7ae46fefe6405b8bdd49f79
Sergio <trugove@hotmail.com>
2017-03-06 12:19:01 +0100

    Road to models

--- sobres/isobres/models.py
+++ sobres/isobres/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Sobre(models.Model):
+    amount = models.IntegerField()
+    concept = models.TextField(max_length=100)
+    date = models.DateTimeField()
